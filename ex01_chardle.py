c1f5b8f76212f676af16d09af021f432a118f0f9
Erica Ocbu <ericao@ad.unc.edu>
2023-01-24 18:54:00 -0500

    Progress on Exercise 1

---
+++ ex01_chardle.py
@@ -0,0 +1,43 @@
+"""EX01 - Chardle - A cute step toward Wordle."""
+
+__author__ = "730560123"
+
+user_word: str = input("Enter a 5-character word: ")
+if len(user_word) != 5:
+    print("Error: Word must contain 5 characters")
+    exit()
+
+user_character: str = input("Enter a single character: ")
+if len(user_character) != 1:
+    print("Error: Word must contain a single character.")
+    exit()
+
+print("Searching for " + user_character + " in " + user_word)
+
+
+character_count: int = 0
+
+if (user_word[0] == user_character):
+    print(user_character + " found at index 0")
+    character_count = character_count + 1
+if (user_word[1] == user_character):
+    print(user_character + " found at index 1")
+    character_count = character_count + 1
+if (user_word[2] == user_character):
+    print(user_character + " found at index 2")
+    character_count = character_count + 1
+if (user_word[3] == user_character):
+    print(user_character + " found at index 3")
+    character_count = character_count + 1
+if (user_word[4] == user_character):
+    print(user_character + " found at index 4")
+    character_count = character_count + 1
+
+if (character_count == 0):
+    print("No instance of " + user_character + " found in " + user_word)
+else:
+    if (character_count > 1):
+        print(str(character_count) + " instances of " + user_character + " found in " + user_word)
+    else:
+        print("1 instance of " + user_character + " found in " + user_word)
+
